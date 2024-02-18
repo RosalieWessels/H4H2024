9b18ae144c29e0e3a4dd281598c91fe0d9003cbd
RosalieWessels <rosalie.wessels@yahoo.com>
2024-02-17 16:39:20 -0800

    Can pull items from firebase and show them on the screen dynamically

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getStorage } from "firebase/storage";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+// import { getStorage } from "firebase/storage";
 
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,6 +15,58 @@ storageBucket: 'santa-clara-swap.appspot.com'
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+firebase.initializeApp(firebaseConfig);
+db = firebase.firestore();
+storage = firebase.storage()
+
+// Get a reference to the storage service, which is used to create references in your storage bucket
+//const storage = getStorage();
+
+// Create a storage reference from our storage service
+// const storageRef = ref(storage);
+var storageRef = firebase.storage().ref();
+
+const uid = function(){
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
+
+console.log(uid())
+
+function createCard() {
+    db.collection("items")
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log(doc.id, " => ", doc.data());
+            var imageName = doc.data().image;
+            var spaceRef = storageRef.child(imageName);
+            spaceRef.getDownloadURL().then(function(url) {
+                // Handle the download URL (e.g., display the image in an <img> tag)
+                console.log('Download URL:', url);
+                var cards = document.getElementById("cards");
+                cards.innerHTML += 
+                `<div class="card">
+                    <div class="card__content">
+                        <img class="card__img" src="${url}">
+                        <h1 class="card__header newsreader-800">${doc.data().title} - $${doc.data().price}</h1>
+                        <p class="card__text newsreader-400" >${doc.data().description}</p>
+                        <button class="pill-button">${doc.data().category}</button> 
+                        <button class="pill-button">${doc.data().availability}</button>
+                        <button class="card__btn newsreader-800" style="margin-top: 10px;">Add to Cart<span>&rarr;</span></button>
+                    </div>
+                </div>`
+            }).catch(function(error) {
+                // Handle any errors
+                console.error('Error retrieving image:', error);
+            });
+        });
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+
+    console.log("INSIDE OF DOWNLOAD IMAGE FUNCTION");
+}
+
+
+createCard();
